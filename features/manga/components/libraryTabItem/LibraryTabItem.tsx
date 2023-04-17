dcc60b026e0be7098ab24ad167d62ace7b9a4083
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2023-04-17 20:19:46 +1000

    feat: :sparkles: add library page

--- features/manga/components/libraryTabItem/LibraryTabItem.tsx
+++ features/manga/components/libraryTabItem/LibraryTabItem.tsx
@@ -2,19 +2,25 @@ import TabiItem from "@Common/components/tab/TabItem";
 import MangaViewSelect from "../mangaViewSelect/MangaViewSelection";
 import { mangaTitles } from "@Manga/data/manga.data";
 import TitleCards from "../cards/titleCards/TitleCards";
+import { MangaTitle } from "@Manga/interfaces/manga.interfaces";
+import { MangaViewSelectionType } from "@Manga/types/manga.types";
 
 interface ILibraryTabItem {
   id: string;
-
+  data: MangaTitle[];
+  selectedView: MangaViewSelectionType;
   defaultTab?: boolean;
 }
 
-const LibraryTabItem = ({ id, defaultTab = false }: ILibraryTabItem) => {
-  const data = id === "dropped" ? [] : mangaTitles;
-
+const LibraryTabItem = ({
+  id,
+  data,
+  defaultTab = false,
+  selectedView,
+}: ILibraryTabItem) => {
   return (
     <TabiItem id={id} defaultTab={defaultTab}>
-      <TitleCards data={data} selectedView="list" />
+      <TitleCards data={data} selectedView={selectedView} />
     </TabiItem>
   );
 };
--- pages/library/index.tsx
+++ pages/library/index.tsx
@@ -6,12 +6,15 @@ import MangaViewSelect from "@Manga/components/mangaViewSelect/MangaViewSelectio
 import { librayTabData } from "@Manga/data/library.data";
 import { mangaTitles } from "@Manga/data/manga.data";
 import { MangaTitle } from "@Manga/interfaces/manga.interfaces";
+import { MangaViewSelectionType } from "@Manga/types/manga.types";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const Library = () => {
   const { query } = useRouter();
   const [data, setData] = useState<MangaTitle[]>([]);
+  const [selectedView, setSelectedView] =
+    useState<MangaViewSelectionType>("list");
 
   useEffect(() => {
     if (typeof query.tab === "string" && query.tab !== "dropped") {
@@ -27,19 +30,40 @@ const Library = () => {
     return `${count} ${title}`;
   };
 
+  const handleSelectedViewChange = (chosenView: MangaViewSelectionType) => {
+    setSelectedView(chosenView);
+  };
+
   return (
     <Container>
       <Section title="Library" hasBack>
         <Tab tabs={librayTabData} />
         <div className="flex items-center justify-between my-4">
           <span className="text-lg">{formatTitle(data.length)}</span>
-          <MangaViewSelect />
+          <MangaViewSelect onSelectedChange={handleSelectedViewChange} />
         </div>
-        <LibraryTabItem id="reading" defaultTab />
-        <LibraryTabItem id="plan-to-read" />
-        <LibraryTabItem id="completed" />
-        <LibraryTabItem id="re-reading" />
-        <LibraryTabItem id="dropped" />
+        <LibraryTabItem
+          id="reading"
+          defaultTab
+          selectedView={selectedView}
+          data={data}
+        />
+        <LibraryTabItem
+          id="plan-to-read"
+          selectedView={selectedView}
+          data={data}
+        />
+        <LibraryTabItem
+          id="completed"
+          selectedView={selectedView}
+          data={data}
+        />
+        <LibraryTabItem
+          id="re-reading"
+          selectedView={selectedView}
+          data={data}
+        />
+        <LibraryTabItem id="dropped" selectedView={selectedView} data={data} />
       </Section>
     </Container>
   );

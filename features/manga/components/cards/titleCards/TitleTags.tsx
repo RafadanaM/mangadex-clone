6777a6984405f23eeba03b6aa8bc35f97f9c7f2a
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2023-04-27 19:20:08 +1000

    refactor: :art: make styling in TitleTag cleaner

--- features/manga/components/cards/titleCards/TitleTags.tsx
+++ features/manga/components/cards/titleCards/TitleTags.tsx
@@ -12,23 +12,21 @@ const TitleTags = forwardRef<HTMLDivElement | null, ITitleTags>(
     const [isOpen, setIsOpen] = useState(false);
 
     function handleClickShowMore() {
-      console.log("asd");
-
       setIsOpen(true);
     }
+
+    const containerStyle = {
+      isOpen: isOpen
+        ? ""
+        : "overflow-y-hidden max-h-[calc(3em_+_0.5rem)] sm:max-h-[1em]",
+      showMore: showMore ? "relative pr-9" : "",
+    };
 
     return (
       <div
         ref={ref}
         // setting max height is important to determine when to add MORE button and hide the wrapped elements(how rows that we want), on smaller screen we add 0.25rem to consider gap-1 which is 0.25rem
-        className={`flex overflow-y-hidden flex-wrap gap-1
-      ${showMore ? "relative pr-9 " : ""}
-      ${
-        !isOpen
-          ? "overflow-y-hidden max-h-[calc(2em_+_0.25rem)] sm:max-h-[1em]"
-          : ""
-      }
-      ${className}`}
+        className={`flex overflow-y-hidden flex-wrap gap-1 ${containerStyle.showMore} ${containerStyle.isOpen} ${className}`}
       >
         {tags.map((tag, idx) => (
           <Tag key={idx}>{tag}</Tag>
